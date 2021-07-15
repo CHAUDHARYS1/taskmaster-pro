e6ba386b48506f0e8406fc24a8f98e40b84fc044
Shital Chaudhary <shitalchau10@gmail.com>
2021-07-15 07:27:10 -0500

    use moment.js for due date and auto color tasks based on urgency

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,40 @@ var tasks = {};
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+  // create span element for task item due date
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+  // create p element for task item description
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
+  // html view - <li> --> <span> --> <p>
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
+
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");}
+};
+
 
 var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -136,8 +156,8 @@ $(".list-group").on("click", "span", function () {
 
   // enable jquery ui datepicker
   dateInput.datepicker({
-    minDate: 1, 
-    onClose: function(){
+    minDate: 1,
+    onClose: function () {
       // when calendar is closed, force a "change" event on the 'dateInput'
       $(this).trigger("change");
     }
@@ -165,10 +185,11 @@ $(".list-group").on("change", "input[type='text']", function () {
   saveTasks();
 
   // recreate span and insert in place of input element
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
   $(this).replaceWith(taskSpan);
+  
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
